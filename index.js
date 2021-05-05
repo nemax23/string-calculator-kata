ad5517dd320112fd79bb918727b61300ff0822ea
Massimiliano <massimiliano@wonderflow.ai>
2021-05-05 17:19:37 +0200

    regexp implementation for step 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const add = (str) => {
   if (str === '') {
     return 0
   }
-  return str.split(',').reduce((acc, current) => acc + parseInt(current), 0)
+  return str.split(/\n|,/).reduce((acc, current) => acc + parseInt(current), 0)
 }
 
 module.exports = {
