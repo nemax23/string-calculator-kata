7117e6878fe92ed391035575411d2fcc4545d05b
Massimiliano <massimiliano@wonderflow.ai>
2021-05-05 17:28:11 +0200

    refactor for supporting custom delimeter

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const add = (str) => {
   if (str === '') {
     return 0
   }
+  const match = str.match(/^\/\/(.*)\n(.*)$/)
+  if (match) {
+    return match[2].split(match[1]).reduce((acc, current) => acc + parseInt(current), 0)
+  }
   return str.split(/\n|,/).reduce((acc, current) => acc + parseInt(current), 0)
 }
 
