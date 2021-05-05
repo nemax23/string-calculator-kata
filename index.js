1d046bf510d8f6d992312488022305d7a700a6cb
Massimiliano <massimiliano@wonderflow.ai>
2021-05-05 17:41:27 +0200

    support of negative numbers throw

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ const add = (str) => {
     return 0
   }
   const match = str.match(/^\/\/(.*)\n(.*)$/)
-  if (match) {
-    return sumByDelimeter(match[2], match[1])
+
+  const { sum, lowers } = match ? sumByDelimeter(match[2], match[1]) : sumByDelimeter(str, /\n|,/)
+  if (lowers.length) {
+    throw new Error(`negatives not allowed: ${lowers.join(',')}`)
   }
-  return sumByDelimeter(str, /\n|,/)
+  return sum
 }
 
-const sumByDelimeter = (str, delimeter) => str.split(delimeter).reduce((acc, current) => acc + parseInt(current), 0)
+const sumByDelimeter = (str, delimeter) => str.split(delimeter).reduce((acc, current) => {
+  const number = parseInt(current)
+  if (number < 0) {
+    acc.lowers.push(number)
+  }
+  acc.sum += number
+  return acc
+}, {
+  sum: 0,
+  lowers: []
+})
 
 module.exports = {
   add
