154b1bf0fcc22b4b8ac3e93b856bdd1f072244ed
Massimiliano <massimiliano@wonderflow.ai>
2021-05-05 17:44:39 +0200

    refactor code

--- index.js
+++ index.js
@@ -3,23 +3,29 @@ const add = (str) => {
   if (str === '') {
     return 0
   }
+
   const match = str.match(/^\/\/(.*)\n(.*)$/)
 
   const { sum, lowers } = match ? sumByDelimeter(match[2], match[1]) : sumByDelimeter(str, /\n|,/)
+
   if (lowers.length) {
     throw new Error(`negatives not allowed: ${lowers.join(',')}`)
   }
+
   return sum
 }
 
 const sumByDelimeter = (str, delimeter) => str.split(delimeter).reduce((acc, current) => {
   const number = parseInt(current)
+
   if (number < 0) {
     acc.lowers.push(number)
   }
+
   if (number < 1000) {
     acc.sum += number
   }
+
   return acc
 }, {
   sum: 0,
